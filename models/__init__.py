73accf8e92c73a55efa62950213429ba92fcca30
jbwang1997 <jbwang1997@gmail.com>
2024-10-14 01:51:42 +0000

    background

--- models/__init__.py
+++ models/__init__.py
@@ -5,5 +5,7 @@ from .opus import OPUS
 from .opus_head import OPUSHead
 from .opus_transformer import OPUSTransformer
 
+from .opus_head_bg import OPUSHeadBG
+
 
 __all__ = ['OPUS', 'OPUSHead', 'OPUSTransformer']
