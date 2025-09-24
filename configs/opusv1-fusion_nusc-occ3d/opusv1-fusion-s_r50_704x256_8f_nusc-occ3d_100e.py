56b867b7f52756bbfc46e0709b8bae90f415299c
jbwang1997 <jbwang1997@gmail.com>
2025-09-24 06:05:03 +0000

    Fix bugs

--- configs/opusv1-fusion_nusc-occ3d/opusv1-fusion-s_r50_704x256_8f_nusc-occ3d_100e.py
+++ configs/opusv1-fusion_nusc-occ3d/opusv1-fusion-s_r50_704x256_8f_nusc-occ3d_100e.py
@@ -177,7 +177,7 @@ test_pipeline = [
     dict(type='LoadPointsFromFile', coord_type='LIDAR', load_dim=5, use_dim=5),
     dict(type='LoadPointsFromMultiSweeps', sweeps_num=9, use_dim=[0, 1, 2, 3, 4],
          pad_empty_sweeps=True, remove_close=True),
-    dict(type='PointsFromLiDARToEgo'),
+    dict(type='LiDARToOccSpace'),
     dict(type='RandomTransformImage', ida_aug_conf=ida_aug_conf, training=False),
     dict(type='PointsRangeFilter', point_cloud_range=point_cloud_range),
     dict(
